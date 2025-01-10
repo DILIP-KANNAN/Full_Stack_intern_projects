fb54c54bc17304bb4710455dd2f4758d0dff53f0
Dilip Kannan K <231501040@rajalakshmi.edu.in>
2025-01-10 16:51:38 +0530

    Add files via upload

---
+++ 1st_Assignment/script.js
@@ -0,0 +1,12 @@
+var num1=10,num2=20,num3=30,num4=40,num5=50;
+var result;
+result= num1+num2+num3+num4+num5;
+result=result-40;
+result*=30;
+console.log(result);
+function calc(){
+    result= num1+num2+num3+num4+num5;
+    result=result-40;
+    result*=30;
+    console.log(result);
+}
